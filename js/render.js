5f264c973411739fad5f83b95cdc7400b2ae8c7d
Mikle <mmigalenko@mail.ru>
2020-11-01 21:57:35 +0300

    вынесение значения в константы в render.js

--- js/render.js
+++ js/render.js
@@ -3,6 +3,7 @@
 const LOW_PRICE_LIMIT = 10000;
 const HIGH_PRICE_START = 50000;
 const PINS_LIMIT = 5;
+const DEFAULT_FILTER = 'any';
 
 const filters = document.querySelector('.map__filters');
 const filtersSelectList = filters.querySelectorAll('select');
@@ -11,7 +12,7 @@ const filtersSelectList = filters.querySelectorAll('select');
  * функция выдачи ранга каждому объявлению, исходя из которого идет сортировка всех объявлений
  * @param {Object} - объявление
  * @param {Array} - массив фильтров чекбоксов
- * @return {Number} - обявление//null
+ * @return {Number} - обявление/null
  */
 const checkAd = (ad, filtersList) => {
   let isSame = true;
@@ -25,19 +26,19 @@ const checkAd = (ad, filtersList) => {
     priceValue = 'high';
   }
 
-  if (filtersSelectList[0].value !== ad.offer.type && filtersSelectList[0].value !== 'any') {
+  if (filtersSelectList[0].value !== ad.offer.type && filtersSelectList[0].value !== DEFAULT_FILTER) {
     isSame = false;
   }
 
-  if (filtersSelectList[1].value !== priceValue && filtersSelectList[1].value !== 'any') {
+  if (filtersSelectList[1].value !== priceValue && filtersSelectList[1].value !== DEFAULT_FILTER) {
     isSame = false;
   }
 
-  if (+filtersSelectList[2].value !== ad.offer.rooms && filtersSelectList[2].value !== 'any') {
+  if (+filtersSelectList[2].value !== ad.offer.rooms && filtersSelectList[2].value !== DEFAULT_FILTER) {
     isSame = false;
   }
 
-  if (+filtersSelectList[3].value !== ad.offer.guests && filtersSelectList[3].value !== 'any') {
+  if (+filtersSelectList[3].value !== ad.offer.guests && filtersSelectList[3].value !== DEFAULT_FILTER) {
     isSame = false;
   }
 
